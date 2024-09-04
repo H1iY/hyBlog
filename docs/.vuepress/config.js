79aa70d13a79199652081d49c3540ba1dc678b09
linmu <linmu@tsign.cn>
2024-09-04 11:54:00 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,41 @@
+module.exports = {
+	base: '/', // 你的网站基础路径
+    title: '江南一点雨',
+    head: [
+        ['link', {rel: 'icon', href: 'https://open.weixin.qq.com/qr/code?username=a_javaboy'}]
+    ],
+    themeConfig: {
+        logo: 'https://open.weixin.qq.com/qr/code?username=a_javaboy',
+        nav: [ 
+            {text: '首页', link: '/'},
+			{text: '国际站', link: 'http://www.javaboy.org'},
+			{text: '国内站', link: 'http://www.itboyhub.com'},
+			{text: '其他', items: [
+				{text: '国际站', link: 'http://www.javaboy.org'},
+				{text: '国内站', link: 'http://www.itboyhub.com'}
+			]}
+        ],
+        sidebar: [
+			{
+				title: 'Java',
+				path: '/2.java/',
+				collapsable: false,
+				sidebarDepth: 2,
+				children: [
+					'/2.java/java入门',
+					'/2.java/java上手',
+				]
+			},
+			{
+				title: 'vue',
+				path: '/1.vue/',
+				collapsable: false,
+				sidebarDepth: 2,
+				children: [
+					'/1.vue/vue入门',
+					'/1.vue/vue上手',
+				]
+			},
+		]
+    },
+};

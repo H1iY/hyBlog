3e689ceb3c6e539879ef9c252f79d0396cb62762
linmu <linmu@tsign.cn>
2024-09-11 09:33:36 +0800

    1231321

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,17 @@
+import { defaultTheme } from 'vuepress'
 module.exports = {
 	base: '/hyBlog/', // 你的网站基础路径
-    title: '江南一点雨',
-    head: [
-        ['link', {rel: 'icon', href: 'https://open.weixin.qq.com/qr/code?username=a_javaboy'}]
-    ],
-    themeConfig: {
-        logo: 'https://open.weixin.qq.com/qr/code?username=a_javaboy',
-        nav: [ 
-            {text: '首页', link: '/'},
-			{text: '国际站', link: 'http://www.javaboy.org'},
-			{text: '国内站', link: 'http://www.itboyhub.com'},
-			{text: '其他', items: [
-				{text: '国际站', link: 'http://www.javaboy.org'},
-				{text: '国内站', link: 'http://www.itboyhub.com'}
-			]}
-        ],
-        sidebar: {
-			'/2.java/': [
+	title: '江南一点雨',
+	head: [
+		['link', { rel: 'icon', href: 'https://open.weixin.qq.com/qr/code?username=a_javaboy' }]
+	],
+	theme: defaultTheme(
+		{
+			logo: 'https://open.weixin.qq.com/qr/code?username=a_javaboy',
+			nav: [
+				{ text: '首页', link: '/' },
+				{ text: '国际站', link: 'http://www.javaboy.org' },
+				{ text: '国内站', link: 'http://www.itboyhub.com' },
 				{
 					title: 'Java',
 					collapsable: true,
